148909d6b953c06547e19672c279aa1ae5bfb18c
oExcellent <waltm3@mail.broward.edu>
2017-07-18 16:46:26 -0400

    Connected objects

--- MyFirstProject/ViewController.swift
+++ MyFirstProject/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var tbx1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btn1(_ sender: Any) {
+        lbl1.text = tbx1.text
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
