1cd6af112c2d6db94a992cd579583fd0abdaec62
oExcellent <waltm3@mail.broward.edu>
2017-07-18 17:47:48 -0400

    Allowed return key to dismiss keyboard and call the action button

--- MyFirstProject/ViewController.swift
+++ MyFirstProject/ViewController.swift
@@ -8,19 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
     @IBOutlet weak var lbl1: UILabel!
     @IBOutlet weak var tbx1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        //return button dismisses keyboard
+      //  tbx1.delegate = self
+    /*
+         func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+         tbx1.resignFirstResponder()
+         return false
+         }
+    */
     }
 
     @IBAction func btn1(_ sender: Any) {
         lbl1.text = tbx1.text
         tbx1.resignFirstResponder()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        tbx1.resignFirstResponder()
+        //buttonCopy()
+        btn1(self)
+        return false
+    }
+    
+    //Copy the button funtion so that it can be used as an all in one keyboard dismisser and action caller.
+    
+    func buttonCopy(){
+    lbl1.text = tbx1.text
+    tbx1.resignFirstResponder()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
